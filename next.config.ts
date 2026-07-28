78abdf091a24ad35e07f214b295754871e308b03
memoowi <megatenlike@gmail.com>
2026-07-28 23:26:05 +0700

    fix: no more shesh

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-  basePath: "/rapor-index", // Remove or change this if you are using a custom domain or a different repository name
+  // /* config options here */
+  // output: "export",
+  // basePath: "/rapor-index", // Remove or change this if you are using a custom domain or a different repository name
 };
 
 export default nextConfig;
