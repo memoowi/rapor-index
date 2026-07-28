975700715ebe40bea4328b6cd00d6f5379af7050
memoowi <megatenlike@gmail.com>
2026-07-28 23:12:51 +0700

    feat: add GitHub Actions workflow for deploying Next.js site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/rapor-index", // Remove or change this if you are using a custom domain or a different repository name
 };
 
 export default nextConfig;
